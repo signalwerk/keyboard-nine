0a4eb76776b5ac809fb5e3a9b46105a688221d52
CI System <sh@signalwerk.ch>
2020-01-12 22:03:05 +0000

    Deploy to GitHub Pages: 5e6fedc6251ce426e47fccc65b15c9715c54775a

---
+++ precache-manifest.272da17c50b7e7547c9f974c1ccb0197.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a6235145dc095978dab8827dc438a8e",
+    "url": "/keyboard-nine/index.html"
+  },
+  {
+    "revision": "f5ea1ad72bdbd817d592",
+    "url": "/keyboard-nine/static/css/main.4fd83121.chunk.css"
+  },
+  {
+    "revision": "6cf42408491b826b0d94",
+    "url": "/keyboard-nine/static/js/2.908aeea9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/keyboard-nine/static/js/2.908aeea9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5ea1ad72bdbd817d592",
+    "url": "/keyboard-nine/static/js/main.7a686295.chunk.js"
+  },
+  {
+    "revision": "6cf0ab0276b0ba7d5829",
+    "url": "/keyboard-nine/static/js/runtime-main.97afe163.js"
+  }
+]);
